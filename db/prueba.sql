90322c48c66d271652a82245c6b2f849f97e85ca
b.gonzalez <braisgonzalez737@gmail.com>
2025-04-01 16:16:29 +0200

    db creada

---
+++ db/prueba.sql
@@ -0,0 +1,63 @@
+CREATE DATABASE IF NOT EXISTS Proyecto1;
+
+USE Proyecto1;
+
+
+CREATE TABLE Productos(
+    id_producto int auto_increment primary key,
+    nombre varchar(30) not null,
+);
+
+CREATE TABLE Ubicacion(
+    id_ubicacion varchar(5) primary key,
+    pais varchar(50) not null,
+    ciudad varchar(50) not null;
+    direccion varchar(50) not null,
+);
+
+
+CREATE TABLE PrecioProducto(
+    id_producto int auto_increment primary key,
+    id_ubicacion varchar(5) primary key,
+    precio DECIMAL(10, 2),
+    FOREIGN KEY (id_producto) REFERENCES Productos(id_producto) ON DELETE CASCADE
+    FOREIGN KEY (id_ubicacion) REFERENCES Ubicacion(id_ubicacion) ON DELETE CASCADE
+)
+
+CREATE TABLE Tienda(
+    id int auto_increment primary key,
+    id_ubicacion varchar(5) not null,
+    id_producto int auto_increment,
+    FOREIGN KEY (id_producto) REFERENCES Productos(id_producto) ON DELETE CASCADE
+    FOREIGN KEY (id_ubicacion) REFERENCES Ubicacion(id_ubicacion) ON DELETE CASCADE
+);
+
+
+INSERT INTO Ubicacion(id_ubicacion, pais, ciudad, direccion) values
+("A1", "Argentina", "Buenos Aires",  "Alfredo R. Bufano 2701-2799"),
+("A2", "Argentina", "Córdoba", "Av. Vélez Sarsfield"),
+("E1", "España", "A Coruña", "Rúa Río Brexa, 5 "),
+("F1", "Francia", "Marsella", "Grands-Carmes 13002 Marsella"),
+("P1", "Portugal", "Lisboa", "Rua da Bempostinha 1");
+
+INSERT INTO Tienda(id_pais) values
+("A1"),
+("A2"),
+("E1"),
+("F1"),
+("P1");
+
+INSERT INTO Productos(nombre) values
+("Zumos"),
+("Bebidas energéticas"),
+("Agua"),
+("Patatas"),
+("Fruta");
+
+INSERT INTO PrecioProducto(id_ubicacion, precio) values
+("A1", 7.9),
+("A2", 10.0),
+("E1", 3.9),
+("F1", 5.5),
+("P1", 3.2);
+    
