dfe40c6a23e16f9903b6e3eeb6d0b82cb067f7aa
jorge.alvarez <jorge220604@gmail.com>
2025-04-04 08:51:58 +0200

    Pasado a inglés

--- db/prueba.sql
+++ db/prueba.sql
@@ -1,57 +1,57 @@
-CREATE DATABASE IF NOT EXISTS Proyecto1;
+CREATE DATABASE IF NOT EXISTS Proyect1;
 
-USE Proyecto1;
+USE Proyect1;
 
 
-CREATE TABLE productos (
-    id_producto SERIAL primary key,
-    nombre varchar(30) not null
+CREATE TABLE products (
+    idProduct SERIAL primary key,
+    name varchar(30) not null
 );
 
-CREATE TABLE ubicacion(
-    id_ubicacion varchar(5) primary key,
-    pais varchar(50) not null,
-    ciudad varchar(50) not null,
-    direccion varchar(50) not null
+CREATE TABLE location(
+    locationId varchar(5) primary key,
+    country varchar(50) not null,
+    city varchar(50) not null,
+    address varchar(50) not null
 );
 
-CREATE TABLE tienda (
-    id_shop SERIAL primary key,
-    id_ubicacion varchar(5) not null,
-    FOREIGN KEY (id_ubicacion) REFERENCES ubicacion(id_ubicacion) ON DELETE CASCADE
+CREATE TABLE shop (
+    shopId SERIAL primary key,
+    locationId varchar(5) not null,
+    FOREIGN KEY (locationId) REFERENCES location(locationId) ON DELETE CASCADE
 );
 
-CREATE TABLE precioProducto(
-    id_producto int  not null,
-    id_ubicacion varchar(5) not null,
-    precio varchar(15) not null,
-    primary key (id_producto,id_ubicacion),
-    FOREIGN KEY (id_producto) REFERENCES productos(id_producto) ON DELETE CASCADE,
-    FOREIGN KEY (id_ubicacion) REFERENCES ubicacion(id_ubicacion) ON DELETE CASCADE
-);id
+CREATE TABLE productPrice(
+    productId int not null,
+    locationId varchar(5) not null,
+    price varchar(15) not null,
+    primary key (productId, locationId),
+    FOREIGN KEY (productId) REFERENCES products(productId) ON DELETE CASCADE,
+    FOREIGN KEY (locationId) REFERENCES location(locationId) ON DELETE CASCADE
+);
 
-INSERT INTO ubicacion(id_ubicacion, pais, ciudad, direccion) values
+INSERT INTO location(locationId, country, city, address) values
 ('A1', 'Argentina', 'Buenos Aires',  'Alfredo R. Bufano 2701-2799'),
 ('A2', 'Argentina', 'Córdoba', 'Av. Vélez Sarsfield'),
 ('E1', 'España', 'A Coruña', 'Rúa Río Brexa, 5 '),
 ('F1', 'Francia', 'Marsella', 'Grands-Carmes 13002 Marsella'),
 ('P1', 'Portugal', 'Lisboa', 'Rua da Bempostinha 1');
 
-INSERT INTO tienda(id_ubicacion) values
+INSERT INTO shop(locationId) values
 ('A1'),
 ('A2'),
 ('E1'),
 ('F1'),
 ('P1');
 
-INSERT INTO productos(nombre) values
+INSERT INTO products(name) values
 ('Zumos'),
 ('Bebidas energéticas'),
 ('Agua'),
 ('Patatas'),
 ('Fruta');
 
-INSERT INTO precioProducto (id_producto, id_ubicacion, precio) VALUES
+INSERT INTO productPrice (productId, locationId, price) VALUES
 (1, 'A1', 7.9),
 (2, 'A2', 10.0),
 (3, 'E1', 3.9),
