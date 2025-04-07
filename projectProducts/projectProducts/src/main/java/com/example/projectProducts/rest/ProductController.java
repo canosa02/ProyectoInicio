ae465ea3aae836f1bc1fa2bd75e39281a6543dbb
jorge.alvarez <jorge220604@gmail.com>
2025-04-07 10:22:10 +0200

    BigDecimal en vez de double

--- projectProducts/projectProducts/src/main/java/com/example/projectProducts/rest/ProductController.java
+++ projectProducts/projectProducts/src/main/java/com/example/projectProducts/rest/ProductController.java
@@ -1,23 +1,26 @@
 package com.example.projectProducts.rest;
 
 import com.example.projectProducts.modelo.ProductModel;
+import com.example.projectProducts.modelo.ProductPriceModel;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.math.BigDecimal;
 
 @RestController
 @RequestMapping("/product")
 public class ProductController {
 
     private List<ProductModel> products = new ArrayList<>();
+    private List<ProductPriceModel> productprices = new ArrayList<>();
 
     public ProductController(){
         products.add(new ProductModel(1,"Pizza con piña"));
         products.add(new ProductModel(2,"Agua"));
         products.add(new ProductModel(1,"Agua con gas"));
         products.add(new ProductModel(1,"Naranjas"));
-
+        productprices.add(new ProductPriceModel(new BigDecimal("7.40"), "A1", 1));
     }
 
     @GetMapping("/{id}")
